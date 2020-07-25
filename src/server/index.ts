9fdc567ecebe870b822138276d05a49495830005
Zaver <zaverclub@gmail.com>
2020-07-25 17:13:48 +0300

    !!! add src directory
    Теперь все файлы в папке src. Это нужно для того, чтобы в будущем
    собирать вебпаком

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,20 @@
 // Environment variables
-require("dotenv").config();
-const {PORT} = process.env;
+require("dotenv");
+const PORT = process.env.PORT || 3000;
 
 // Imports
-//const express = require("express");
 import express, {Application, Request, Response} from 'express';
 
 // Creating express server
 const app: Application = express();
+
 
 // On server run
 app.listen(PORT,
     ()=>console.log(`Listening on port ${PORT}`)
 );
 
+// Routes
 app.get('/', (req: Request, res: Response) => {
-    res.send("<h1>залуподрищенск</h1>")
+    res.sendFile('/home/be9st/WebstormProjects/pure-chat/src/client/index.html')
 })
